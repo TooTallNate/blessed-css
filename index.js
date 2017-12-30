eca194aff3858e21da8c610c140bbcfafef0e9c4
Nathan Rajlich <nathan@tootallnate.net>
2017-12-29 23:53:45 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,133 @@
+const extend = require('deep-extend')
+const CSSselect = require('css-select')
+const { parseDOM } = require('htmlparser2')
+const cssparser = require('cssparser/lib/cssparser')
+const { calculate: calculateSpecificity } = require('specificity')
+
+module.exports = createStyle
+
+function deleteEmpty(o) {
+  for (const k of Object.keys(o)) {
+    if (o[k] == null) {
+      delete o[k]
+    } else if (typeof o[k] === 'object') {
+      deleteEmpty(o[k])
+    }
+  }
+}
+
+function toHTML(container, pseudo, self = false, child = '') {
+  const name = container.constructor.name.toLowerCase()
+  let className = ''
+  if (container.options.classNames) {
+    className = encodeURIComponent(
+      Array.from(container.options.classNames).join(' ')
+    )
+  }
+  if (pseudo) {
+    className += ` __pseudo_${pseudo}`
+  }
+  const html = `<${name} class="${className}" ${self ? 'self' : ''}>${child}</${
+    name
+  }>`
+  if (container.parent) {
+    return toHTML(container.parent, null, false, html)
+  } else {
+    return html
+  }
+}
+
+function createStyle(css) {
+  // create new instance of Parser
+  const parser = new cssparser.Parser()
+
+  // parse
+  const ast = parser.parse(css)
+
+  // getting json
+  const json = ast.toJSON('simple')
+  //console.log(json.value)
+
+  const rules = []
+  const specificities = new Map()
+  for (const rule of json.value) {
+    //console.error(rule)
+    if (rule.type !== 'rule') {
+      continue
+    }
+
+    rules.push(rule)
+
+    for (const selector of rule.selectors) {
+      // https://www.w3.org/TR/css3-selectors/#specificity
+      specificities.set(
+        selector,
+        parseInt(
+          calculateSpecificity(selector)[0]
+            .specificityArray.slice(1) // skip [0] because we don't consider inline styles
+            .join(''),
+          10
+        )
+      )
+    }
+  }
+  //console.log(specificities)
+  //console.log(Array.from(specificities.keys()))
+
+  function getStyle(container, pseudo = null) {
+    const html = toHTML(container, pseudo, true)
+    //console.log(html)
+
+    const [dom] = parseDOM(html)
+
+    const [self] = CSSselect.selectAll('[self]', [dom])
+    //console.log(self)
+
+    const matches = new Map()
+    for (const rule of rules) {
+      for (const _selector of rule.selectors) {
+        try {
+          const selector = _selector.replace(':', '.__pseudo_')
+          //console.error(selector)
+          const match = CSSselect.selectAll(selector, [dom]).some(
+            match => match === self
+          )
+          if (match) {
+            matches.set(_selector, rule)
+            //console.log(rule)
+          }
+        } catch (err) {
+          console.error(_selector, err)
+        }
+      }
+    }
+    //console.log(matches)
+
+    const sorted = Array.from(matches.keys())
+      .sort((a, b) => {
+        return specificities.get(a) - specificities.get(b)
+      })
+      .map(key => matches.get(key).declarations)
+    //console.log(sorted)
+
+    let s = container.style
+    if (pseudo) {
+      s = s[pseudo]
+    }
+    const current = extend({}, s)
+    deleteEmpty(current)
+
+    //console.log({ pseudo, current })
+    return extend({}, ...sorted, current)
+  }
+
+  function addStyle(container) {
+    container.style = getStyle(container)
+    container.style.border = getStyle(container, 'border')
+    container.style.focus = getStyle(container, 'focus')
+    container.style.hover = getStyle(container, 'hover')
+    container.style.scrollbar = getStyle(container, 'scrollbar')
+  }
+
+  return addStyle
+}
