578dea0b628a72d1e0398ebb87adf5b5b0a54b94
Nathan Rajlich <nathan@tootallnate.net>
2019-11-22 18:14:45 -0800

    100% coverage for `hover.ts`

--- test/hover.test.ts
+++ test/hover.test.ts
@@ -16,12 +16,12 @@ test('"mouseenter"/"mouseleave" events', async () => {
 	let mouseenterCount = 0;
 	let mouseleaveCount = 0;
 	const screen = blessed.screen();
+	initHover(screen);
 	try {
 		const box = blessed.box({
 			parent: screen,
 			clickable: true
 		});
-		initHover(screen);
 		box.on('mouseenter', () => mouseenterCount++);
 		box.on('mouseleave', () => mouseleaveCount++);
 		screen.render();
@@ -40,6 +40,31 @@ test('"mouseenter"/"mouseleave" events', async () => {
 		screen.emit('mousemove', { x: 10000, y: 10000 });
 		assert.equal(mouseenterCount, 1);
 		assert.equal(mouseleaveCount, 1);
+	} finally {
+		screen.destroy();
+	}
+});
+
+test('skip hidden box', async () => {
+	let mouseenterCount = 0;
+	let mouseleaveCount = 0;
+	const screen = blessed.screen();
+	initHover(screen);
+	try {
+		const box = blessed.box({
+			parent: screen,
+			clickable: true,
+			hidden: true
+		});
+		box.on('mouseenter', () => mouseenterCount++);
+		box.on('mouseleave', () => mouseleaveCount++);
+		screen.render();
+
+		assert.equal(box.visible, false);
+		screen.emit('mousemove', { x: 0, y: 0 });
+		screen.emit('mousemove', { x: 10000, y: 10000 });
+		assert.equal(mouseenterCount, 0);
+		assert.equal(mouseleaveCount, 0);
 	} finally {
 		screen.destroy();
 	}
